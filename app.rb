53500980f66d7b1f08ba2f34ebec0e05e061c4f4
JosephPR <reilly4181@gmail.com>
2019-05-13 00:18:49 -0600

    added songs

--- app.rb
+++ app.rb
@@ -10,7 +10,15 @@ require 'sinatra/reloader'
               {title: 'New York, New York', url: "https://www.youtube.com/watch?v=btFfXgUdIzY"},
               {title: 'My Way', url: 'https://www.youtube.com/watch?v=6E2hYDIFDIU'},
               {title: 'The Way You Look Tonight', url: 'https://www.youtube.com/watch?v=h9ZGKALMMuc'},
-              {title: 'I Get A Kick Out Of You', url: 'https://www.youtube.com/watch?v=wSrHvNr8QQQ'}]
+              {title: 'I Get A Kick Out Of You', url: 'https://www.youtube.com/watch?v=wSrHvNr8QQQ'},
+              {title: 'Luck be a lady', url: 'https://www.youtube.com/watch?v=MfiKk4wxiVM'},
+              {title: 'Nice and easy', url: 'https://www.youtube.com/watch?v=e2dDijEJC_g'},
+              {title: 'Thats Life', url: 'https://www.youtube.com/watch?v=KIiUqfxFttM'},
+              {title: 'Killing Me Softly', url: 'https://www.youtube.com/watch?v=8tbP3f3i03E'},
+              {title: 'Come Fly With Me', url: 'https://www.youtube.com/watch?v=HmQq6yLe2ww'},
+              {title: 'Summer Wind', url: 'https://www.youtube.com/watch?v=nqISqqbMhls&list=RDEMDaSUSUVu68QDj2nSmWsorA&index=10'},
+              {title:  'Ive Got You Under My Skin', url: 'https://www.youtube.com/watch?v=7P3TuCfjX_w&list=RDEMDaSUSUVu68QDj2nSmWsorA&index=14'},
+              {title: 'Blue Moon', url: 'https://www.youtube.com/watch?v=Dw1ZC6sZjIY&list=RDEMDaSUSUVu68QDj2nSmWsorA&index=26'}]
     erb :default
   end
 
